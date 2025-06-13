d8cf659a091b066e7e5c999c914f84f05c18f9fd
Luih <luis.valdes1@virginiogomez.cl>
2025-06-13 10:12:16 -0400

    Add files via upload

---
+++ ejercicio 3.py
@@ -0,0 +1,24 @@
+#numeros que desea ingresar el usuario
+numeros = input("Ingresa aqui tus numeros: ")
+#eliminar números con el  primer negativo
+numeros_filtrados = []
+for n in numeros.split():
+    if int(n) < 0:
+        break
+    numeros_filtrados.append(n)
+#para convertir en numeros enteros y guardarlos en una lista
+lista = [int(n) for n in numeros_filtrados]
+#encontrar numero mayor y menor 
+if lista:
+    mayor = lista[0]
+    menor = lista[0]
+    for n in lista[1:]: #este sirve para recorrer la lista desde el segundo elemento
+        if n > mayor:
+            mayor = n
+        if n < menor:
+            menor = n
+    #Aqui para mostrar el resultado
+    print(f"El numero mayor es: {mayor}")
+    print(f"El numero menor es: {menor}")
+else:
+    print("No se ingresaron números válidos.")
